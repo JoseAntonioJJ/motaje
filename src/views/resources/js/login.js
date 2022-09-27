7d0a092242c62638e42ede87105210785b1b7044
Jose Antonio <joseantonio@email.de.mentira.com>
2022-09-28 00:28:21 +0200

    cuando se pulsa citas se dirige a la pantalla de login. Se ha simulado la consulta para que darle algo de funcionalidad

---
+++ src/views/resources/js/login.js
@@ -0,0 +1,49 @@
+//definimos la variables que vamos a usar
+let userString = '';
+let passwordString = '';
+
+// guardamos los elementos del DOM en variables
+const usuario = document.getElementById('user');
+const pass = document.getElementById('pass');
+const enviar = document.getElementById('enviar');
+const recibido = document.getElementById('recibido')
+
+//añadimos eventos que cada vez que el cliente escribe (tipea) lo captura
+usuario.addEventListener('keyup',()=>{
+    //en esta funcion recogemos el valor del input usuario
+    userString = usuario.value;
+    
+});
+
+pass.addEventListener('keyup',()=>{
+    //en esta funcion recogemos el valor del input pass
+    passwordString = pass.value;
+    
+});
+
+//añadimos un evento que cuando el elemento id = enviar es clickado envia a consulta a BD si existe el usuario con esa contraseña
+enviar.addEventListener('click',()=>{
+    console.log('enviando datos');
+    console.log('usuario:' + userString);
+    console.log('pass:' + passwordString);
+
+    let data = new FormData();
+    data.append('usuario',userString);
+    data.append('password',passwordString);
+
+    fetch('/src/services/consulta.php',{
+    method : 'POST',
+    body : data
+})
+.then((response)=>{
+    if(response.ok) { return response.text(); }else{ throw 'Error en la llamada';}
+})
+.then ((texto)=>{
+    recibido.innerHTML=texto;
+})
+.catch((fallo)=>{
+    enviar.innerHTML('Algo ha fallado')
+})
+
+});
+
